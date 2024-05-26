11465a72aeba2165f550d7e4d29646709a96aa46
TranNhanThe <nhanthe133@gmail.com>
2024-05-26 12:36:25 +0700

    remove first  allow origin

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Origin', 'https://todoclient-20475861z-trannhanthes-projects.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
